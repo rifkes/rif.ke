246a866b9a8e73582a5fd763d9b69f07bceb9c10
Rifkes <rifke@rifke.world>
2023-09-23 14:11:03 +0100

    Refactor touchcanvas into its own file

--- next/src/components/3D/PhotoDistortAndGrain.jsx
+++ next/src/components/3D/PhotoDistortAndGrain.jsx
@@ -6,6 +6,7 @@ import * as THREE from 'three';
 import { lerp } from 'three/src/math/MathUtils';
 import { useSiteGlobals } from '@/utils/SiteGlobalsContext.jsx';
 import useWindowSize from '@/hooks/useWindowSize.jsx';
+import TouchCanvas from './TouchCanvas.jsx';
 
 const vert = `
   varying vec2 vUv;
@@ -52,8 +53,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
   const touchPoint = useRef(new THREE.Vector2(.5, .65));
   const targetTouchPoint = useRef(new THREE.Vector2(.5, .65));
   const touchCanvasPoint = useRef([ touchPoint.current.x * window.innerWidth, (1 - touchPoint.current.y) * window.innerHeight ]);
-  const isMoving = useRef(false);
-  const movingTimer = useRef(null);
   const touchCanvas = useRef(null);
   const touchCanvasCtx = useRef(null);
   const texture1Canvas = useRef(null);
@@ -62,6 +61,7 @@ const PhotoDistortAndGrain = ({ stage }) => {
   const texture2CanvasCtx = useRef(null);
   const vignetteCanvas = useRef(null);
   const vignetteCanvasCtx = useRef(null);
+  const vignetteOpacity = useRef(1.0);
   const touchTexture = useRef(null);
   const texture1 = useRef(null);
   const texture2 = useRef(null);
@@ -75,20 +75,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
   useEffect(() => {
     currentStage.current = stage;
   }, [ stage ]);
-
-  // initialise the touchcanvas configuration
-  useEffect(() => {
-    for (let i = 0; i < config.current.dotsNumber; i++) {
-      touchTrail.current[ i ] = {
-        x: touchCanvasPoint.current[ 0 ],
-        y: touchCanvasPoint.current[ 1 ],
-        vx: 0,
-        vy: 0,
-        intensity: i ? .9 * (config.current.dotsNumber - i) / config.current.dotsNumber : .9,
-        r: config.current.dotsBaseRadius * (1 + Math.pow(i / config.current.dotsNumber, .9))
-      };
-    }
-  }, []);
 
   const handleDrawImage = useCallback((img, visualCanvas, visualCtx, textureToChange) => {
     const cw = visualCanvas.width;
@@ -140,12 +126,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
 
   // initialise the image canvases
   useEffect(() => {
-    const newTouchCanvas = document.createElement('canvas');
-    newTouchCanvas.width = window.innerWidth;
-    newTouchCanvas.height = window.innerHeight;
-    touchCanvas.current = newTouchCanvas;
-    touchCanvasCtx.current = newTouchCanvas.getContext('2d');
-    touchTexture.current = new THREE.CanvasTexture(touchCanvas.current);
 
     const newTexture1Canvas = document.createElement('canvas');
     newTexture1Canvas.width = window.innerWidth;
@@ -185,43 +165,17 @@ const PhotoDistortAndGrain = ({ stage }) => {
     // document.body.appendChild(texture2Canvas.current);
     // document.body.appendChild(touchCanvas.current);
 
-    touchCanvasCtx.current.fillStyle = 'black';
-    touchCanvasCtx.current.fillRect(0, 0, touchCanvas.current.width, touchCanvas.current.height);
-
     const vignette = vignetteCanvasCtx.current.createRadialGradient(
       512 / 2, 512 / 2, Math.min(512, 512) / 3,
       512 / 2, 512 / 2, Math.min(512, 512) / 2,
     );
 
-    vignette.addColorStop(0, `rgba(0, 0, 0, ${ 0 })`);
-    vignette.addColorStop(1, `rgba(255, 255, 255, 255)`);
+    vignette.addColorStop(0, `rgba(255, 255, 255, ${ 0 })`);
+    vignette.addColorStop(1, `rgba(255, 255, 255, ${ vignetteOpacity.current })`);
 
     vignetteCanvasCtx.current.fillStyle = vignette;
     vignetteCanvasCtx.current.fillRect(0, 0, 512, 512);
-
-    let movingTimeout = () => {
-      isMoving.current = false;
-    };
-    movingTimer.current = setTimeout(movingTimeout, 300);
-
-    window.addEventListener('mousemove', (e) => {
-      updateMousePosition(e.clientX, e.clientY);
-    });
-    window.addEventListener('touchmove', (e) => {
-      updateMousePosition(e.targetTouches[ 0 ].pageX, e.targetTouches[ 0 ].pageY);
-    });
-
-    function updateMousePosition(eX, eY) {
-      if (isMoving.current === false) {
-        isMoving.current = true;
-      }
-      clearTimeout(movingTimer.current);
-      movingTimer.current = setTimeout(movingTimeout, 300);
-
-      targetTouchPoint.current.x = eX / window.innerWidth;
-      targetTouchPoint.current.y = 1.1 - Math.max(eY / window.innerHeight, .2);
-    }
-  }, [handleDrawImage]);
+  }, [ handleDrawImage ]);
 
 
 
@@ -239,8 +193,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
     u_noise_seed: { type: 'f', value: noiseSeed },
     u_distortion_amount: { type: 'f', value: 1.0 },
   });
-
-  const [ color ] = useState('255, 255, 255');
 
   const activeTextureIndex = useRef(0);
   const currentFadeAmount = useRef(0);
@@ -334,73 +286,13 @@ const PhotoDistortAndGrain = ({ stage }) => {
 
   }, []);
 
-  // update touchCanvas
-  const updateTrail = useCallback(() => {
-    if (!touchCanvasCtx.current || !touchTexture.current) return;
-    if (targetDistortionAmount.current === 0) {
-      touchCanvasCtx.current.fillStyle = 'rgba(0, 0, 0, .1)';
-    } else {
-      touchCanvasCtx.current.fillStyle = 'rgba(0, 0, 0, .01)';
-    }
-    touchCanvasCtx.current.fillRect(0, 0, touchCanvas.current.width, touchCanvas.current.height);
-
-    touchTrail.current.forEach((p, pIdx) => {
-      if (pIdx === 0) {
-        p.x = touchCanvasPoint.current[ 0 ];
-        p.y = touchCanvasPoint.current[ 1 ];
-      } else {
-        p.vx += (touchTrail.current[ pIdx - 1 ].x - p.x) * config.current.tailSpring;
-        p.vx *= config.current.tailFriction;
-
-        p.vy += (touchTrail.current[ pIdx - 1 ].y - p.y) * config.current.tailSpring;
-        p.vy += config.current.tailGravity;
-        p.vy *= config.current.tailFriction;
-
-        p.x += p.vx;
-        p.y += p.vy;
-      }
-
-      const grd = touchCanvasCtx.current.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.r);
-      grd.addColorStop(0, `rgba(${ color },${ 0.1 })`);
-      grd.addColorStop(1, `rgba(${ color }, 0)`);
-
-      touchCanvasCtx.current.beginPath();
-      touchCanvasCtx.current.fillStyle = grd;
-      touchCanvasCtx.current.arc(p.x, p.y, p.r, 0, Math.PI * 2);
-      touchCanvasCtx.current.fill();
-    });
-
-    touchTexture.current.needsUpdate = true;
-    texture1.current.needsUpdate = true;
-    texture2.current.needsUpdate = true;
-  }, [ color ]);
-
   useFrame(({ clock }) => {
-    touchPoint.current.x += (targetTouchPoint.current.x - touchPoint.current.x) * config.current.catchingSpeed;
-    touchPoint.current.y += (targetTouchPoint.current.y - touchPoint.current.y) * config.current.catchingSpeed;
-    touchCanvasPoint.current = [ touchPoint.current.x * window.innerWidth, (1 - touchPoint.current.y) * window.innerHeight ];
-
     const time = clock.getElapsedTime();
-
-    if (isMoving.current) {
-      config.current.tailGravity -= .1;
-      config.current.tailGravity = Math.max(config.current.tailGravity, config.current.tailGravityBonds[0]);
-    } else {
-      config.current.tailGravity += .1;
-      config.current.tailGravity = Math.min(config.current.tailGravity, config.current.tailGravityBonds[1]);
-    }
-    config.current.tailGravity += .12 * Math.sin(3 * time);
     currentFadeAmount.current = lerp(currentFadeAmount.current, targetFadeAmount.current, 0.05);
-
     currentDistortionAmount.current = lerp(currentDistortionAmount.current, targetDistortionAmount.current, 0.05);
-
-    updateTrail();
     if (material.current?.uniforms) {
-      material.current.uniforms.u_touch_texture.value = touchTexture.current;
       material.current.uniforms.u_transition_amount.value = currentFadeAmount.current;
       material.current.uniforms.u_time.value = time;
-      material.current.uniforms.u_mouse.value.x = touchPoint.current.x;
-      material.current.uniforms.u_mouse.value.y = touchPoint.current.y;
       material.current.uniforms.u_distortion_amount.value = currentDistortionAmount.current;
     }
   });
@@ -511,6 +403,7 @@ const PhotoDistortAndGrain = ({ stage }) => {
   }, []);
 
   return (
+    <>
     <mesh
       ref={ mesh }
       position={[0, 0, -200]}
@@ -525,7 +418,13 @@ const PhotoDistortAndGrain = ({ stage }) => {
         depthWrite={ false }
         depthTest={ false }
       />
-    </mesh>
+      </mesh>
+      <TouchCanvas { ...{
+        touchTrail, touchCanvasPoint, config, touchTexture, touchCanvas,
+        touchCanvasCtx, material, touchPoint, targetTouchPoint, targetDistortionAmount,
+        texture1, texture2,
+      } } />
+    </>
   );
 }
 
--- next/src/pages/index.js
+++ next/src/pages/index.js
@@ -5,15 +5,15 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <h1 className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center p-2 mix-blend-difference text-white'>Rifke</h1>
-      <button className='fixed top-0 left-1/2 -translate-x-1/2 p-2 uppercase mix-blend-difference text-white'>
+      <h1 className='select-none fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center p-2 mix-blend-difference text-white'>Rifke</h1>
+      <button className='select-none fixed top-0 left-1/2 -translate-x-1/2 p-2 uppercase mix-blend-difference text-white'>
         Info
       </button>
-      <button className='fixed bottom-0 left-1/2 -translate-x-1/2 text-center p-2 uppercase mix-blend-difference text-white'>
+      <button className='select-none fixed bottom-0 left-1/2 -translate-x-1/2 text-center p-2 uppercase mix-blend-difference text-white'>
         Ask me anything
       </button>
-      <button className='fixed top-1/2 left-0 p-2 uppercase mix-blend-difference text-white'>Previous</button>
-      <button className='fixed top-1/2 right-0 p-2 uppercase mix-blend-difference text-white'>Next</button>
+      <button className='select-none fixed top-1/2 -translate-y-1/2 left-0 p-2 uppercase mix-blend-difference text-white'>Previous</button>
+      <button className='select-none fixed top-1/2 -translate-y-1/2 right-0 p-2 uppercase mix-blend-difference text-white'>Next</button>
     </main>
   )
 }
