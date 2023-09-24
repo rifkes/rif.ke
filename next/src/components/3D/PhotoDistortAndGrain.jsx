399232c51a4ee7c4eed0ed0001523e98aab7a977
Rifkes <rifke@rifke.world>
2023-09-24 02:43:29 +0100

    Refactor the webcam texture

--- next/src/components/3D/PhotoDistortAndGrain.jsx
+++ next/src/components/3D/PhotoDistortAndGrain.jsx
@@ -8,6 +8,7 @@ import { useSiteGlobals } from '@/utils/SiteGlobalsContext.jsx';
 import useWindowSize from '@/hooks/useWindowSize.jsx';
 import TouchCanvas from './TouchCanvas.jsx';
 import VignetteCanvas from './VignetteCanvas.jsx';
+import WebcamTexture from './WebcamTexture.jsx';
 
 const vert = `
   varying vec2 vUv;
@@ -261,13 +262,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
       img.src = backgroundImageHistory.current[0];
     }
   }, [ windowWidth, windowHeight ]);
-  
-  useEffect(() => {
-
-
-
-
-  }, []);
 
   useFrame(({ clock }) => {
     const time = clock.getElapsedTime();
@@ -279,72 +273,6 @@ const PhotoDistortAndGrain = ({ stage }) => {
       material.current.uniforms.u_distortion_amount.value = currentDistortionAmount.current;
     }
   });
-
-  
-
-  // start recording from the webcam
-  useEffect(() => {
-    let raf;
-    
-				// We can't `new Video()` yet, so we'll resort to the vintage
-				// "hidden div" hack for dynamic loading.
-				var streamContainer = document.createElement('div');
-				streamContainer.classList.add('stream__container');
-				const video = document.createElement('video')
-				video.classList.add('stream');
-
-				// If we don't do this, the stream will not be played.
-				// By the way, the play and pause controls work as usual
-				// for streamed videos.
-				video.setAttribute('autoplay', '1')
-				video.setAttribute('playsinline', '1') // important for iPhones
-
-				// The video should fill out all of the canvas
-				video.setAttribute('width', 0.5)
-				video.setAttribute('height', 0.5)
-
-				// streamContainer.appendChild(video)
-				// document.body.appendChild(streamContainer)
-
-				// As soon as we can draw a new frame on the canvas, we call the `draw` function
-				// we passed as a parameter.
-				const update = function() {
-					var last = Date.now()
-					const loop = () => {
-						// For some effects, you might want to know how much time is passed
-						// since the last frame; that's why we pass along a Delta time `dt`
-						// variable (expressed in milliseconds)
-						var dt = Date.now() - last
-						// self.callback(self.video, dt)
-            last = Date.now()
-            texture1CanvasCtx.current.drawImage(video, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
-            texture2CanvasCtx.current.drawImage(video, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
-
-            texture1CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
-            texture2CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture2Canvas.current.width, texture2Canvas.current.height);
-            texture1.current.needsUpdate = true;
-            texture2.current.needsUpdate = true;
-						raf = requestAnimationFrame(loop)
-					}
-					raf = requestAnimationFrame(loop)
-				}
-
-				// The callback happens when we are starting to stream the video.
-				navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function(stream) {
-					// Yay, now our webcam input is treated as a normal video and
-					// we can start having fun
-          video.srcObject = stream;
-          update();
-				}, function(err) {
-					throw err
-				}).catch( (error) => {
-					console.log(error);
-				})
-
-    return () => {
-      cancelAnimationFrame(raf);
-    }
-  }, []);
 
   useEffect(() => {
 
@@ -410,6 +338,7 @@ const PhotoDistortAndGrain = ({ stage }) => {
         texture1, texture2,
       } } />
       <VignetteCanvas { ...{ vignetteCanvas, vignetteCanvasCtx, targetVignetteOpacity, } } />
+      <WebcamTexture { ...{ texture1, texture2, texture1Canvas, texture2Canvas, texture1CanvasCtx, texture2CanvasCtx, vignetteCanvas } } />
     </>
   );
 }
--- next/src/components/3D/WebcamTexture.jsx
+++ next/src/components/3D/WebcamTexture.jsx
@@ -1,83 +1,76 @@
 import { useSiteGlobals } from '@/utils/SiteGlobalsContext';
 import { useEffect, useRef, useState } from 'react';
 
-const WebcamTexture = ({ texture1Canvas, texture2Canvas, texture1CanvasCtx, texture2CanvasCtx, vignetteCanvas, texture1, texture2 }) => {
+const WebcamTexture = ({ texture1, texture2, texture1Canvas, texture2Canvas, texture1CanvasCtx, texture2CanvasCtx, vignetteCanvas }) => {
+
+
+
   
-  const { backgroundImage } = useSiteGlobals();
-  const [ isInitialised, setIsInitialised ] = useState(false);
-  const video = useRef(document.createElement('video'));
 
+  // start recording from the webcam
   useEffect(() => {
-
     let raf;
+      // We can't `new Video()` yet, so we'll resort to the vintage
+      // "hidden div" hack for dynamic loading.
+      var streamContainer = document.createElement('div');
+      streamContainer.classList.add('stream__container');
+      const video = document.createElement('video')
+      video.classList.add('stream');
 
-    console.log(video.current);
+      // If we don't do this, the stream will not be played.
+      // By the way, the play and pause controls work as usual
+      // for streamed videos.
+      video.setAttribute('autoplay', '1')
+      video.setAttribute('playsinline', '1'); // important for iPhones
+      video.setAttribute('muted', '1');
 
-    const update = function () {
-      // As soon as we can draw a new frame on the canvas, we call the `draw` function
-      // we passed as a parameter.
-    console.log('WebcamTexture: useEffect: start recording from the webcam');
-      let last = Date.now();
-      const loop = () => {
-      console.log('WebcamTexture: useEffect: start recording from the webcam');
-        // For some effects, you might want to know how much time is passed
-        // since the last frame; that's why we pass along a Delta time `dt`
-        // variable (expressed in milliseconds)
-        const dt = Date.now() - last;
-        // self.callback(self.video, dt)
-        last = Date.now();
-        texture1CanvasCtx.current.drawImage(video.current, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
-        texture2CanvasCtx.current.drawImage(video.current, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
+				// The video should fill out all of the canvas
+				video.setAttribute('width', 0.5)
+				video.setAttribute('height', 0.5)
 
-        texture1CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
-        texture2CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture2Canvas.current.width, texture2Canvas.current.height);
-        texture1.current.needsUpdate = true;
-        texture2.current.needsUpdate = true;
-        raf = requestAnimationFrame(loop);
-      };
-      raf = requestAnimationFrame(loop);
-    };
+				// streamContainer.appendChild(video)
+				// document.body.appendChild(streamContainer)
 
-    if (backgroundImage === 'webcam' && isInitialised === true) {
-      raf = requestAnimationFrame(update);
-    }
+				// As soon as we can draw a new frame on the canvas, we call the `draw` function
+				// we passed as a parameter.
+				const update = function() {
+					var last = Date.now()
+					const loop = () => {
+						// For some effects, you might want to know how much time is passed
+						// since the last frame; that's why we pass along a Delta time `dt`
+						// variable (expressed in milliseconds)
+						var dt = Date.now() - last
+						// self.callback(self.video, dt)
+            last = Date.now()
+            texture1CanvasCtx.current.drawImage(video, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
+            texture2CanvasCtx.current.drawImage(video, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
+
+            texture1CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture1Canvas.current.width, texture1Canvas.current.height);
+            texture2CanvasCtx.current.drawImage(vignetteCanvas.current, 0, 0, vignetteCanvas.current.width, vignetteCanvas.current.height, 0, 0, texture2Canvas.current.width, texture2Canvas.current.height);
+            texture1.current.needsUpdate = true;
+            texture2.current.needsUpdate = true;
+						raf = requestAnimationFrame(loop)
+					}
+					raf = requestAnimationFrame(loop)
+				}
+
+				// The callback happens when we are starting to stream the video.
+				navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function(stream) {
+					// Yay, now our webcam input is treated as a normal video and
+					// we can start having fun
+          video.srcObject = stream;
+          update();
+				}, function(err) {
+					throw err
+				}).catch( (error) => {
+					console.log(error);
+				})
 
     return () => {
       cancelAnimationFrame(raf);
     }
-  }, [ backgroundImage ]);
-
-  // start recording from the webcam
-  useEffect(() => {
-    // We can't `new Video()` yet, so we'll resort to the vintage
-    // "hidden div" hack for dynamic loading.
-
-    // If we don't do this, the stream will not be played.
-    // By the way, the play and pause controls work as usual
-    // for streamed videos.
-    video.current.setAttribute('autoplay', '1');
-    video.current.setAttribute('playsinline', '1'); // important for iPhones
-
-    // The video should fill out all of the canvas
-    video.current.setAttribute('width', 0.5);
-    video.current.setAttribute('height', 0.5);
-
-
-    // The callback happens when we are starting to stream the video.
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) {
-      console.log('video stream', stream)
-      // Yay, now our webcam input is treated as a normal video and
-      // we can start having fun
-      video.current.srcObject = stream;
-      setIsInitialised(true);
-    }, function (err) {
-      throw err;
-    }).catch((error) => {
-      console.log(error);
-    });
-    
-  }, [ backgroundImage, texture1Canvas, texture2Canvas, texture1CanvasCtx, texture2CanvasCtx, vignetteCanvas, texture1, texture2 ]);
-
+  }, []);
+  
   return null;
 };
 
