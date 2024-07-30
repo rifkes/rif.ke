a841ebc1df0e5884a7cb94a9c1635113ae3d4379
Rifkes <rifke@rifke.world>
2024-07-30 13:31:06 +0100

    Remove console log

--- next/src/utils/Seo.jsx
+++ next/src/utils/Seo.jsx
@@ -5,60 +5,54 @@ const Seo = (props) => {
   const { seoImage, title, seoDescription, image, } = props;
   const { siteGlobals } = useSiteGlobals();
 
-  console.log('Seo siteGlobals:', siteGlobals);
-
-  if (siteGlobals?.settings) {
-    return (
-      <Head>
-        {
-          <title>{ `${ siteGlobals?.settings?.siteTitle ? siteGlobals.settings.siteTitle : 'Rifke' }${ title ? ` | ${ title }` : '' }` }</title>
-        }
-        {
-          siteGlobals.settings.seoTags &&
-          <meta name="keywords" content={ `${ siteGlobals.settings.seoTags.join(', ') }` } />
-        }
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {
-          siteGlobals.settings?.favicon?.url ?
-            <link rel="icon" href={ siteGlobals.settings.favicon.url } /> :
-            <link rel="icon" href="/favicon.ico" />
-        }
-        {
-          siteGlobals?.settings?.seoDescription &&
-          <>
-            <meta name="description" content={ siteGlobals.settings.seoDescription } />
-            <meta property='og:description' content={ siteGlobals.settings.seoDescription } />
-            <meta name='twitter:description' content={ siteGlobals.settings.seoDescription } />
-          </>
-        }
-        {
-          seoImage?.url ?
-          <>
-            <meta property="og:image" content={ seoImage.url } />
-            <meta name="twitter:image" content={ seoImage.url } />
-          </>
-            :
-          image?.url ?
-          <>
-            <meta property="og:image" content={ image.url } />
-            <meta name="twitter:image" content={ image.url } />
-          </>
-            :
-          siteGlobals?.settings?.seoImage?.url ?
-          <>
-            <meta property="og:image" content={ siteGlobals?.settings?.seoImage.url } />
-            <meta name="twitter:image" content={ siteGlobals?.settings?.seoImage.url } />
-          </>
-            :
-          <></>
-        }
-        <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <Head>
+      {
+        <title>{ `${ siteGlobals?.settings?.siteTitle ? siteGlobals.settings.siteTitle : 'Rifke' }${ title ? ` | ${ title }` : '' }` }</title>
+      }
+      {
+        siteGlobals?.settings?.seoTags &&
+        <meta name="keywords" content={ `${ siteGlobals.settings.seoTags.join(', ') }` } />
+      }
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {
+        siteGlobals?.settings?.favicon?.url ?
+          <link rel="icon" href={ siteGlobals.settings.favicon.url } /> :
+          <link rel="icon" href="/favicon.ico" />
+      }
+      {
+        (seoDescription || siteGlobals?.settings?.seoDescription) &&
+        <>
+          <meta name="description" content={ seoDescription ?? siteGlobals.settings.seoDescription } />
+          <meta property='og:description' content={ seoDescription ?? siteGlobals.settings.seoDescription } />
+          <meta name='twitter:description' content={ seoDescription ?? siteGlobals.settings.seoDescription } />
+        </>
+      }
+      {
+        seoImage?.url ?
+        <>
+          <meta property="og:image" content={ seoImage.url } />
+          <meta name="twitter:image" content={ seoImage.url } />
+        </>
+          :
+        image?.url ?
+        <>
+          <meta property="og:image" content={ image.url } />
+          <meta name="twitter:image" content={ image.url } />
+        </>
+          :
+        siteGlobals?.settings?.seoImage?.url ?
+        <>
+          <meta property="og:image" content={ siteGlobals?.settings?.seoImage.url } />
+          <meta name="twitter:image" content={ siteGlobals?.settings?.seoImage.url } />
+        </>
+          :
+        <></>
+      }
+      <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+  )
 };
 
 export default Seo;
